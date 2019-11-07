756e4b9b46395bb53d0d3ed63346e5c30939a843
shibatamasayuki <masayuki.shibata.0831@gmail.com>
2019-11-07 11:06:04 +0900

    C-3まで完成
    完成したものは#にてコメントアウトしてあります｡

---
+++ customer.py
@@ -0,0 +1,101 @@
+'''
+c-1.フルネームを取得できる
+ken = Customer(first_name="Ken", family_name="Tanaka")
+ken.full_name()  # "Ken Tanaka" という値を返す
+
+tom = Customer(first_name="Tom", family_name="Ford")
+tom.full_name()  # "Tom Ford" という値を返す
+'''
+
+# class Customer:
+#     def __init__(self, first_name, family_name):
+#         self.first_name = first_name
+#         self.family_name = family_name
+#
+#     def full_name(self):
+#         print(f'{self.first_name} {self.family_name}')
+#
+#
+# ken = Customer(first_name="Ken", family_name="Tanaka")
+# ken.full_name()
+#
+# tom = Customer(first_name="Tom", family_name="Ford")
+# tom.full_name()
+
+'''
+C-2.年齢という概念の追加
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+ken.age  # 15 という値を返す
+
+tom = Customer(first_name="Tom", family_name="Ford", age= 57)
+tom.age # 57 という値を返す
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+ieyasu.age # 73 という値を返す
+'''
+
+# class Customer:
+#     def __init__(self, first_name, family_name, age):
+#         self.first_name = first_name
+#         self.family_name = family_name
+#         self.age = age
+
+
+# ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+# ken.age
+# print(ken.age)
+
+# tom = Customer(first_name="Tom", family_name="Ford", age=57)
+# tom.age
+# print(tom.age)
+
+# ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+# ieyasu.age
+# print(ieyasu.age)
+
+'''
+C-3. 年齢に応じた適切な入場料(entry_fee)を計算できる
+料金の計算ルール
+こども料金(20歳未満): 1000円
+おとな料金(20歳以上65歳未満): 1500円
+シニア料金(65歳以上): 1200円
+
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+ken.entry_fee()  # 1000 という値を返す
+
+tom = Customer(first_name="Tom", family_name="Ford", age= 57)
+tom.entry_fee() # 1500 という値を返す
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+ieyasu.entry_fee() # 1200 という値を返す
+'''
+
+
+class Customer:
+    def __init__(self, first_name, family_name, age):
+        self.first_name = first_name
+        self.family_name = family_name
+        self.age = age
+
+    def entry_fee(self):
+
+        if self.age < 20:
+            return 1000
+
+        if self.age < 65:
+            return 1500
+
+        return 1200
+
+
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+ken.entry_fee()  # 1000 という値を返す
+print(ken.entry_fee())  # 1000 という値を返す確認用
+
+tom = Customer(first_name="Tom", family_name="Ford", age=57)
+tom.entry_fee()  # 1500 という値を返す
+print(tom.entry_fee())  # 1500 という値を返す確認用
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+ieyasu.entry_fee()  # 1200 という値を返す
+print(ieyasu.entry_fee())  # 1200 という値を返す確認用
