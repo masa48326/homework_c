c64456a167c237ce5f2813e47e063f2a08341bb4
shibatamasayuki <masayuki.shibata.0831@gmail.com>
2019-11-07 13:12:25 +0900

    C-6 まで完了

--- customer.py
+++ customer.py
@@ -105,25 +105,89 @@ ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
 ken.info_csv()  # "Ken Tanaka,15,1000" という値を返す
 
 tom = Customer(first_name="Tom", family_name="Ford", age= 57)
-ken.info_csv()  # "Tom Ford,57,1500" という値を返す
+tom.info_csv()  # "Tom Ford,57,1500" という値を返す
 
 ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
 ieyasu.info_csv()  # "Ieyasu Tokugawa,73,1200" という値を返す
+'''
+# class Customer:
+#     def __init__(self, first_name, family_name, age):
+#         self.first_name = first_name
+#         self.family_name = family_name
+#         self.age = age
+#         self.list = [self.full_name(),str(self.age),str(self.entry_fee())]
+#
+#     def full_name(self):
+#         return f'{self.first_name} {self.family_name}'
+#
+#     def entry_fee(self):
+#         if self.age < 20:
+#             return 1000
+#         if self.age < 65:
+#             return 1500
+#
+#         return 1200
+#
+#     def info_csv(self):
+#         list_csv = ','.join(self.list)
+#         return list_csv
+#
+#
+# ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+# ken.info_csv()  # "Ken Tanaka,15,1000" という値を返す
+# print(ken.info_csv())  # "Ken Tanaka,15,1000" という値を返す確認用
+#
+# tom = Customer(first_name="Tom", family_name="Ford", age= 57)
+# tom.info_csv()  # "Tom Ford,57,1500" という値を返す
+# print(tom.info_csv())  # "Tom Ford,57,1500" という値を返す確認用
+#
+# ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+# ieyasu.info_csv()  # "Ieyasu Tokugawa,73,1200" という値を返す
+# print(ieyasu.info_csv())  # "Ieyasu Tokugawa,73,1200" という値を返す確認用
+
+'''
+C-5. 3歳以下の入場料金の無料化
+3歳以下の入場料金は無料にしてください
+
+C-6. 75歳以上の料金区分の追加
+75歳以上の入場料金は500円にしてください
+
+C-7. 単一顧客の情報取得形式の追加その1
+単一顧客の情報取得をタブ区切りにも対応させてください
+下記は出力の例
+Ken Tanaka    15    1000
+Tom Ford    57    1500
+Ieyasu Tokugawa    73    1200
+
+C-8.単一顧客の情報取得形式の追加その
+単一顧客の情報取得を「|」(パイプ)区切りにも対応させてください
+下記は出力の例
+Ken Tanaka|15|1000
+Tom Ford|57|1500
+Ieyasu Tokugawa|73|1200
 '''
 class Customer:
     def __init__(self, first_name, family_name, age):
         self.first_name = first_name
         self.family_name = family_name
         self.age = age
-        self.full_name = f'{self.first_name} {self.family_name}'
+        self.list = [self.full_name(), str(self.age), str(self.entry_fee())]
+
+    def full_name(self):
+        return f'{self.first_name} {self.family_name}'
 
     def entry_fee(self):
-        if self.age < 20:
+        if self.age = < 3:
+            return 0
+        elif 3 < self.age < 20:
             return 1000
-        if self.age < 65:
+        elif 20 = < self.age < 65:
             return 1500
+        elif 65 = < self.age < 75:
+            return 1200
 
-        return 1200
+        return 500
 
     def info_csv(self):
-        return self.full_name, self.age, self.entry_fee
+        list_csv = ','.join(self.list)
+        return list_csv
